9a5dd77b89c10a3c7e8d6d4c0d1bcafb195f961f
kumarswamyk <68528859+kumarswamyk@users.noreply.github.com>
2022-12-13 22:34:44 +0530

    Initial commit

---
+++ src/components/Navigate.js
@@ -0,0 +1,26 @@
+import { Outlet, Link } from 'react-router-dom';
+import React from 'react';
+
+const Navigate = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigate;
